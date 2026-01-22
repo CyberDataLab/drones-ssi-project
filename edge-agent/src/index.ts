a7b7d4c7d9c7e2025bbf8265ed8ff6dc24fce1d6
Alfonso <alfonsoserra2003@gmail.com>
2026-01-22 12:09:31 +0100

    Check if DID already exists befor creting it

--- edge-agent/src/index.ts
+++ edge-agent/src/index.ts
@@ -4,8 +4,27 @@ async function main() {
     await initializeDatabase()
     console.log('Agent initialized and database connected.')
 
-    const identifier = await agent.didManagerCreate({ alias: 'drone-default' })
-    console.log('DID created:', identifier)
+
+    // 1. Check if there is an existing DID with the alias 'drone-default'
+    const existingDID = await agent.didManagerFind({
+        alias: 'drone-default',
+    })
+
+    let identifier
+
+    if (existingDID.length > 0) {
+        identifier = existingDID[0]
+        console.log('Existing DID found:', identifier.did)
+    } else {
+        identifier = await agent.didManagerCreate({
+            alias: 'drone-default',
+            // provider: 'did:web',
+        })
+        console.log('New DID created: ', identifier.did)
+    }
+
+    console.log(identifier)
+
 }
 
 main().catch(console.error)
