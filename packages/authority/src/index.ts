5ac8e8767a746d4ffd9580399c6af85828ff6c01
Alfonso <alfonsoserra2003@gmail.com>
2026-03-03 11:22:48 +0000

    Changed JWT licenses to BBS+ in the Authority

--- packages/authority/src/index.ts
+++ packages/authority/src/index.ts
@@ -1,6 +1,12 @@
 import { createSSIAgent } from '@tfm/shared'
 import * as readline from 'readline'
 import { v4 as uuidv4 } from 'uuid' 
+import * as fs from 'fs'
+import * as path from 'path'
+import { Bls12381G2KeyPair, BbsBlsSignature2020 } from '@mattrglobal/jsonld-signatures-bbs'
+// @ts-ignore
+import { extendContextLoader, sign, purposes } from 'jsonld-signatures'
+
 
 // Auxiliar function to read input from terminal
 function openTerminal(question: string): Promise<string> {
@@ -16,6 +22,44 @@ function openTerminal(question: string): Promise<string> {
     })
   })
 }
+
+// To optimize cryptographic performance and avoid latency and Rate-Limiting locks on W3C servers, 
+// an in-memory caching mechanism was implemented in the Linked Data resolver engine.
+const contextCache = new Map();
+
+const customLoader = async (url: string) => {
+
+  if (contextCache.has(url)) {
+    console.log(`⚡ [Context Cache Hit]: ${url}`)
+    return contextCache.get(url);
+  } 
+
+  console.log(`🌐 [Loading W3C Context]: ${url}`)
+
+  const response = await fetch(url, {
+    headers: { 'Accept': 'application/ld+json, application/json' },
+    redirect: 'follow'
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error HTTP ${response.status} while fetching context ${url}`)
+  }
+
+  const document = await response.json();
+
+  const result = {
+    contextUrl: null,
+    documentUrl: url,
+    document: document
+  }
+
+  contextCache.set(url, result);
+  return result;
+};
+
+const documentLoader = extendContextLoader(customLoader);
+
+
 
 async function main() {
   console.log('🏛️  Starting Certification Authority (Interactive Mode)')
@@ -38,6 +82,10 @@ async function main() {
     console.log(`✅ Active Authority: ${authorityIdentifier.did}`)
     console.log('-------------------------------------------------------')
 
+
+    console.log('\n⚙️ Generating ney BLS12-381 key pair for signing credentials ZKP...')
+    const keyPair = await Bls12381G2KeyPair.generate();
+
     const targetDID = await openTerminal('👉 Please enter the drone DID (did:key:...): ')
 
     if (!targetDID || !targetDID.startsWith('did:')) {
@@ -45,34 +93,61 @@ async function main() {
         return
     }
 
-    console.log(`\n⚙️  Creating license for: ${targetDID}...`)
-
+    console.log(`\n⚙️  Creating BBS+ license for: ${targetDID}...`)
     // Generate a unique license ID using UUID
     const licenseId = `urn:uuid:${uuidv4()}`;
 
-    // Create and sign the Verifiable Credential with the license information
-    const verifiableCredential = await agent.createVerifiableCredential({
-      credential: {
-        id: licenseId, 
-        issuer: { id: authorityIdentifier.did },
-        credentialSubject: {
-          id: targetDID,
-          type: 'DroneLicense',
-          licenseClass: 'Class-A',
-          expiryDate: '2030-01-01',
-          authorizedArea: 'Madrid-Norte'
-        },
-      },
-      proofFormat: 'jwt',
-      save: true
-    })
 
-    console.log('📜 Credential Created and Signed!')
-    console.log(`🔑 License ID: ${licenseId}`) // Show the ID
+
+    const credentialDocument = {
+      "@context": [
+        "https://www.w3.org/2018/credentials/v1",
+        "https://w3id.org/security/bbs/v1",
+        // Custom attributes for the drone license (JSON-LD context)
+        {
+          "DroneLicense": "https://tfm.es/vocab#DroneLicense",
+          "licenseClass": "https://tfm.es/vocab#licenseClass",
+          "expiryDate": "https://tfm.es/vocab#expiryDate",
+          // This could be used as domain identifiers (check it later)
+          "authorizedArea": "https://tfm.es/vocab#authorizedArea"
+        }
+       ],
+      "id": licenseId,
+      "type": ['VerifiableCredential', 'DroneLicense'],
+      "issuer": authorityIdentifier.did,
+      "issuanceDate": new Date().toISOString(),
+      "credentialSubject": {
+        "id": targetDID,
+        "type": "DroneLicense",
+        "licenseClass": "Class-A",
+        "expiryDate": "2030-01-01",
+        "authorizedArea": "Murcia"
+      }
+    };
+
+
+    const signedCredential = await sign(credentialDocument, {
+      suite: new BbsBlsSignature2020({ key: keyPair }),
+      purpose: new purposes.AssertionProofPurpose(),
+      documentLoader: documentLoader
+    });
+
+    console.log('📜 Credential Created and Signed with BBS+!')
+
+    const shortDID = targetDID.substring(0, 16);
+    const outputFileName = `license-${shortDID}.json`;
+    const outDir = path.join(process.cwd(), 'issued-licenses');
+    
+    if (!fs.existsSync(outDir)) {
+      fs.mkdirSync(outDir);
+    }
+
+    const outputPath = path.join(outDir, outputFileName);
+
     console.log('---------------------------------------------------')
-    console.log(verifiableCredential.proof.jwt)
+    fs.writeFileSync(outputPath, JSON.stringify(signedCredential, null, 2), 'utf-8');
+    console.log(`💾 Signed credential saved to: ${outputPath}`)
     console.log('---------------------------------------------------')
-    console.log('✅ Copy the JWT above and use it in the drone installation script.')
 
   } catch (error) {
     console.error('❌ Error:', error)
